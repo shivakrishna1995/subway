675c150abc1d26ce2256527bc545d33e9da118cf
Shiva Krishna Gopannagari <gopannagarishiva@gmail.com>
2025-09-05 15:32:03 +0100

    implemented location page

---
+++ src/routes/__root.tsx
@@ -0,0 +1,27 @@
+import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
+import { TanstackDevtools } from '@tanstack/react-devtools'
+
+import Header from '../components/Header'
+import Footer from '@/components/Footer'
+
+export const Route = createRootRoute({
+  component: () => (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+      <TanstackDevtools
+        config={{
+          position: 'bottom-left',
+        }}
+        plugins={[
+          {
+            name: 'Tanstack Router',
+            render: <TanStackRouterDevtoolsPanel />,
+          },
+        ]}
+      />
+    </>
+  ),
+})
