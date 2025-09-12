0699745d6812f21513137949f71b0c5542c50d32
Shiva Krishna Gopannagari <gopannagarishiva@gmail.com>
2025-09-12 13:08:52 +0100

    updated zustand

--- src/store/cart.ts
+++ src/store/cart.ts
@@ -18,6 +18,22 @@ export const useCartStore = create<{
         qty: number,
         id: number,
     }[],
+    sides: {
+        item: {
+            productName: string,
+        },
+        itemType: string,
+        qty: number,
+        id: number,
+    }[],
+    drinks: {
+        item: {
+            productName: string,
+        },
+        itemType: string,
+        qty: number,
+        id: number,
+    }[],
     addItem: (item: {
         item: z.infer<typeof OrderSchema>,
         itemType: string,
@@ -32,33 +48,49 @@ export const useCartStore = create<{
         qty: number,
         id: number,
     }) => void,
+    addSide: (item: {
+        item: {
+            productName: string,
+        },
+        itemType: string,
+        qty: number,
+        id: number,
+    }) => void,
+    addDrink: (item: {
+        item: {
+            productName: string,
+        },
+        itemType: string,
+        qty: number,
+        id: number,
+    }) => void,
     removeItem: (id: number) => void,
     removePlatter: (id: number) => void,
+    removeSide: (id: number) => void,
+    removeDrink: (id: number) => void,
     updateQty: (id: number, qty: number) => void,
     updatePlatterQty: (id: number, qty: number) => void,
+    updateSideQty: (id: number, qty: number) => void,
+    updateDrinkQty: (id: number, qty: number) => void,
     clear: () => void
 }>()(
     persist(
         (set, get) => ({
             items: [],
             platters: [],
-            addItem: (item: {
-                item: z.infer<typeof OrderSchema>,
-                itemType: string,
-                qty: number,
-                id: number,
-            }) => set({
+            sides: [],
+            drinks: [],
+            addItem: (item) => set({
                 items: [...get().items, item],
             }),
-            addPlatter: (item: {
-                item: {
-                    productName: string,
-                },
-                itemType: string,
-                qty: number,
-                id: number,
-            }) => set({
+            addPlatter: (item) => set({
                 platters: [...get().platters, item],
+            }),
+            addSide: (item) => set({
+                sides: [...get().sides, item],
+            }),
+            addDrink: (item) => set({
+                drinks: [...get().drinks, item],
             }),
             removeItem: (id: number) => set({
                 items: get().items.filter(item => item.id !== id),
@@ -66,11 +98,23 @@ export const useCartStore = create<{
             removePlatter: (id: number) => set({
                 platters: get().platters.filter(item => item.id !== id),
             }),
+            removeSide: (id: number) => set({
+                sides: get().sides.filter(item => item.id !== id),
+            }),
+            removeDrink: (id: number) => set({
+                drinks: get().drinks.filter(item => item.id !== id),
+            }),
             updateQty: (id: number, qty: number) => set({
                 items: get().items.map(item => item.id === id ? { ...item, qty } : item),
             }),
             updatePlatterQty: (id: number, qty: number) => set({
                 platters: get().platters.map(item => item.id === id ? { ...item, qty } : item),
+            }),
+            updateSideQty: (id: number, qty: number) => set({
+                sides: get().sides.map(item => item.id === id ? { ...item, qty } : item),
+            }),
+            updateDrinkQty: (id: number, qty: number) => set({
+                drinks: get().drinks.map(item => item.id === id ? { ...item, qty } : item),
             }),
             clear: () => set({ items: [], platters: [] }),
         }),
